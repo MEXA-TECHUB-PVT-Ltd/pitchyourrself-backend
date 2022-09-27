113f5f87eefa99b9f5ad8e621e286061615e37f4
RimshaRiaz-22 <rimshanimo22@gmail.com>
2022-09-26 22:30:49 -0700

    final commit

--- apis/user/operations/reportUser/reportPost.js
+++ apis/user/operations/reportUser/reportPost.js
@@ -3,30 +3,20 @@ const app = express()
 const { reportModel, userModel } = require('../../../../schemas')
 
 const CreateUser = app.post('/create-report', (req, res) => {
-    userModel.find({ _id: req.body.userProfileId }, (error, result) => {
+
+    const user = new reportModel({
+        userProfileId: req.body.userProfileId,
+        userPostId: req.body.userPostId,
+        reportReason: req.body.reportReason,
+        reportedById: req.body.reportedById,
+    })
+    user.save((error, result) => {
         if (error) {
             res.send(error)
         } else {
-            if (result === undefined || result.length == 0) {
-                res.send("Check User Profile id")
-            } else {
-                const user = new reportModel({
-                    userProfileId:req.body.userProfileId,
-                    userPostId: req.body.userPostId,
-                    reportReason: req.body.reportReason,
-                    reportedById: req.body.reportedById,
-                })
-                user.save((error, result) => {
-                    if (error) {
-                        res.send(error)
-                    } else {
-                        res.send(result)
-                    }
-                })
-            }
+            res.send(result)
         }
     })
-      
-
 })
+
 module.exports = CreateUser
