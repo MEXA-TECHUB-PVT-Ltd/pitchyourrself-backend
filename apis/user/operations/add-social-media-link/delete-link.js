87081e155123acb79a01417a4ec165c863592a65
RimshaRiaz-22 <rimshanimo22@gmail.com>
2022-10-02 22:38:21 -0700

    final commit

--- apis/user/operations/add-social-media-link/delete-link.js
+++ apis/user/operations/add-social-media-link/delete-link.js
@@ -3,12 +3,20 @@ const app = express()
 const { UserProfileLinksModel } = require('../../../../schemas')
 
 const DeleteAdmin = app.delete('/delete-social-link-user', (req, res) => {
-    UserProfileLinksModel.findByIdAndDelete(req.body._id, (error, result) => {
+    UserProfileLinksModel.find({ userId: req.body.userId, icon: req.body.icon }, (error, result) => {
         if (error) {
             res.send(error)
         } else {
-            res.sendStatus(200)
+            const LinkID = result[0]._id
+            UserProfileLinksModel.findByIdAndDelete(LinkID, (error, result) => {
+                if (error) {
+                    res.send(error)
+                } else {
+                    res.sendStatus(200)
+                }
+            })
         }
     })
+
 })
 module.exports = DeleteAdmin
